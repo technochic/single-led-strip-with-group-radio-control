167bb6f7ec0bb40d389df3272d0351b5d1d8f366
Natasha <technochic@users.noreply.github.com>
2021-10-05 23:52:03 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,33 +23,33 @@ basic.forever(function () {
     } else if (currentMODE == 2) {
         solid(neopixel.hsl(10, 100, 50))
     } else if (currentMODE == 3) {
-    	
+        solid(neopixel.hsl(30, 100, 50))
     } else if (currentMODE == 4) {
-    	
+        solid(neopixel.hsl(58, 100, 50))
     } else if (currentMODE == 5) {
-    	
+        solid(neopixel.hsl(105, 100, 50))
     } else if (currentMODE == 6) {
-    	
+        solid(neopixel.hsl(180, 100, 50))
     } else if (currentMODE == 7) {
-    	
+        solid(neopixel.hsl(235, 100, 50))
     } else if (currentMODE == 8) {
-    	
+        solid(neopixel.hsl(280, 100, 50))
     } else if (currentMODE == 9) {
-    	
+        solid(neopixel.hsl(335, 100, 50))
     } else if (currentMODE == 10) {
-    	
+        solid(neopixel.hsl(235, 100, 50))
     } else if (currentMODE == 12) {
-    	
+        solid(neopixel.hsl(10, 100, 50))
     } else if (currentMODE == 12) {
-    	
+        solid(neopixel.hsl(0, 0, 50))
     } else if (currentMODE == 13) {
-    	
+        solid(neopixel.hsl(335, 100, 50))
     } else if (currentMODE == 14) {
-    	
+        solid(neopixel.hsl(316, 100, 50))
     } else if (currentMODE == 15) {
-    	
+        solid(neopixel.hsl(105, 100, 50))
     } else if (currentMODE == 16) {
-    	
+        solid(neopixel.hsl(0, 0, 0))
     } else {
     	
     }
