83769d60ece23873de25c92cee53dd68dec29c74
Natasha <technochic@users.noreply.github.com>
2021-10-05 22:07:25 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "mode") {
-    	
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
     }
     if (name == "beat") {
     	
     }
 })
+let strip: neopixel.Strip = null
 radio.setGroup(1)
-let strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
 basic.forever(function () {
 	
 })
